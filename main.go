bea5a521431bd0e45280b4eb08c3bdee013da815
Nadia Bahrami <nadia.bahrami@gmail.com>
2017-10-08 11:39:04 -0700

    Pulling URL form request object

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"./twitch"
 	"net/http"
+	// "path"
 	"io/ioutil"
 	"os"
 	"encoding/json"
@@ -16,9 +17,6 @@ func main() {
 
 	// Configure a sample route
 	http.HandleFunc("/users", myTwitchHandler)
-	http.HandleFunc("/channel", myChannelHandler)
-	http.HandleFunc("/streams", myStreamHandler)
-
 
 	// Run your server
 	http.ListenAndServe(":8080", nil)
@@ -28,6 +26,8 @@ func myTwitchHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Recieved the following request:", r.Body)
 	clientID := os.Getenv("CLIENT_ID")
 	twitch.DoSomething()
+	fmt.Println(r.URL)
+	// fmt.Println(path.Base(r))
 
 	// YOUR ROUTES LOGIC GOES HERE
 	//
@@ -37,7 +37,7 @@ func myTwitchHandler(w http.ResponseWriter, r *http.Request) {
 
 	// resp, err := client.Get("https://api.twitch.tv/kraken/users?login=dallas,dallasnchains")
 	
-	request, err := http.NewRequest("GET", "https://api.twitch.tv/kraken/users?login=dallas,dallasnchains", nil)
+	request, err := http.NewRequest("GET", "https://api.twitch.tv/kraken/users?login=dallas", nil)
 	if err != nil {
 		fmt.Println(err.Error())
 	 	os.Exit(1)
@@ -77,21 +77,11 @@ func myTwitchHandler(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(err)
 	}
     fmt.Fprintf(w, string(responseData2))
-}
 
-
-func myChannelHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Recieved the following request:", r.Body)
-	clientID := os.Getenv("CLIENT_ID")
-	// YOUR ROUTES LOGIC GOES HERE
-	//
-	// Feel free to structure your routing however you see fit, this is just an example to get you started.
-
-	client := &http.Client{}
-
-	// resp, err := client.Get("https://api.twitch.tv/kraken/users?login=dallas,dallasnchains")
-	
-	request, err := http.NewRequest("GET", "https://api.twitch.tv/kraken/channels/44322889", nil)
+    fmt.Println("Making Third Request")
+    
+    client = &http.Client{}
+	request, err = http.NewRequest("GET", "https://api.twitch.tv/kraken/streams/" + user_id, nil)
 	if err != nil {
 		fmt.Println(err.Error())
 	 	os.Exit(1)
@@ -99,42 +89,15 @@ func myChannelHandler(w http.ResponseWriter, r *http.Request) {
 	request.Header.Set("Client-ID", clientID)
 	request.Header.Set("Accept", "application/vnd.twitchtv.v5+json")
 
-	resp, err := client.Do(request)
+	resp, err = client.Do(request)
         
-    responseData, err := ioutil.ReadAll(resp.Body)
+    responseData3, err := ioutil.ReadAll(resp.Body)
     if err != nil {
 		log.Fatal(err)
 	}
-    fmt.Fprintf(w, string(responseData))
-}
-
-func myStreamHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Recieved the following request:", r.Body)
-	clientID := os.Getenv("CLIENT_ID")
-	// YOUR ROUTES LOGIC GOES HERE
-	//
-	// Feel free to structure your routing however you see fit, this is just an example to get you started.
-
-	client := &http.Client{}
-
-	// resp, err := client.Get("https://api.twitch.tv/kraken/users?login=dallas,dallasnchains")
-	
-	request, err := http.NewRequest("GET", "https://api.twitch.tv/kraken/streams/44322889", nil)
-	if err != nil {
-		fmt.Println(err.Error())
-	 	os.Exit(1)
-	} 
-	request.Header.Set("Client-ID", clientID)
-	request.Header.Set("Accept", "application/vnd.twitchtv.v5+json")
-
-	resp, err := client.Do(request)
-        
-    responseData, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-		log.Fatal(err)
-	}
-    fmt.Fprintf(w, string(responseData))
-
+	fmt.Println(responseData3)
+	fmt.Fprintf(w, "Stream Status")
+    fmt.Fprintf(w, string(responseData3))
 }
 
 type GetUsersAPIResponse struct {
