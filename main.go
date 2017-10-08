a709416d705a587a99d5de0430fb11d5bb38f15d
Nadia Bahrami <nadia.bahrami@gmail.com>
2017-10-07 17:43:50 -0700

    Channel information now accessible

--- main.go
+++ main.go
@@ -13,7 +13,8 @@ func main() {
 	fmt.Println("Booting the server...")
 
 	// Configure a sample route
-	http.HandleFunc("/", myTwitchHandler)
+	http.HandleFunc("/users", myTwitchHandler)
+	http.HandleFunc("/channel", myChannelHandler)
 
 
 	// Run your server
@@ -34,6 +35,35 @@ func myTwitchHandler(w http.ResponseWriter, r *http.Request) {
 	// resp, err := client.Get("https://api.twitch.tv/kraken/users?login=dallas,dallasnchains")
 	
 	request, err := http.NewRequest("GET", "https://api.twitch.tv/kraken/users?login=dallas,dallasnchains", nil)
+	if err != nil {
+		fmt.Println(err.Error())
+	 	os.Exit(1)
+	} 
+	request.Header.Set("Client-ID", "ow8er2yjqzvbwzvfx832rrb5ucy933")
+	request.Header.Set("Accept", "application/vnd.twitchtv.v5+json")
+
+	resp, err := client.Do(request)
+        
+    responseData, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+		log.Fatal(err)
+	}
+    fmt.Println(string(responseData))
+
+}
+
+func myChannelHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Recieved the following request:", r.Body)
+
+	// YOUR ROUTES LOGIC GOES HERE
+	//
+	// Feel free to structure your routing however you see fit, this is just an example to get you started.
+
+	client := &http.Client{}
+
+	// resp, err := client.Get("https://api.twitch.tv/kraken/users?login=dallas,dallasnchains")
+	
+	request, err := http.NewRequest("GET", "https://api.twitch.tv/kraken/channels/44322889", nil)
 	if err != nil {
 		fmt.Println(err.Error())
 	 	os.Exit(1)
