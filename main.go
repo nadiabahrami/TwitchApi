8e64145e60342aeceb11d4febdd4c423856c64c4
Nadia Bahrami <nadia.bahrami@gmail.com>
2017-10-07 17:34:15 -0700

    Cleaning up - removing test example

--- main.go
+++ main.go
@@ -13,7 +13,6 @@ func main() {
 	fmt.Println("Booting the server...")
 
 	// Configure a sample route
-	http.HandleFunc("/sample_route", myHandlerFunc)
 	http.HandleFunc("/", myTwitchHandler)
 
 
@@ -21,31 +20,10 @@ func main() {
 	http.ListenAndServe(":8080", nil)
 }
 
-// myHandlerFunc - A sample handler function for the route /sample_route for your HTTP server
-func myHandlerFunc(w http.ResponseWriter, r *http.Request) {
+func myTwitchHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Recieved the following request:", r.Body)
 
 	twitch.DoSomething()
-
-	// YOUR ROUTES LOGIC GOES HERE
-	//
-	// Feel free to structure your routing however you see fit, this is just an example to get you started.
-	response, err := http.Get("http://pokeapi.co/api/v2/pokedex/kanto/")
-	if err != nil {
-		fmt.Println(err.Error())
-	 	os.Exit(1)
-	} else{
-        responseData, err := ioutil.ReadAll(response.Body)
-      	if err != nil {
-			log.Fatal(err)
-		}
-        fmt.Println(string(responseData))
-    }
-
-}
-
-func myTwitchHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Recieved the following request:", r.Body)
 
 	// YOUR ROUTES LOGIC GOES HERE
 	//
